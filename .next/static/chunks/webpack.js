e673480ef71158de64de683ad3eff54d610880fc
Sherin <sherin.lin@carplus.com.tw>
2020-12-09 18:14:31 +0800

    1209

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a6cc88288f93a1933ca3";
+/******/ 	var hotCurrentHash = "b1c84a872e4fba43ca4c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/components/inputField.js
@@ -0,0 +1,80 @@
+import TextField from '@material-ui/core/TextField';
+
+export default function InputField({ setFuncErr, setFunc, value, err, placeholder, type, filled }) {
+    function handle(e) {
+        const newValue = e.target.value;
+        if (type === 'name') {
+            setFunc({ ...value, name: newValue });
+            setFuncErr({ ...err, nameErr: nameValidate(newValue) });
+        } else if (type === 'id') {
+            setFunc({ ...value, id: newValue });
+            setFuncErr({ ...err, idErr: idValidate(newValue) });
+        } else if (type === 'phone') {
+            setFunc({ ...value, phone: newValue });
+            setFuncErr({ ...err, phoneErr: phoneValidate(newValue) });
+        } else if (type === 'email') {
+            setFunc({ ...value, email: newValue });
+            setFuncErr({ ...err, emailErr: emailValidate(newValue) });
+        }
+        filled();
+    }
+    return (
+        <TextField
+            fullWidth
+            required
+            placeholder={placeholder}
+            InputProps={{ disableUnderline: true }}
+            value={value.type}
+            onChange={handle}
+        />
+    );
+}
+
+function nameValidate(newValue) {
+    //Name
+    if (newValue && newValue.length < 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function phoneValidate(newValue) {
+    const phonePattern = new RegExp(/^09\d{2}-?\d{3}-?\d{3}$/);
+    if (!phonePattern.test(newValue) && newValue) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function emailValidate(newValue) {
+    const emailPattern = new RegExp(
+        /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+    );
+    if (!emailPattern.test(newValue) && newValue.length > 5 && newValue) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function idValidate(id) {
+    const tab = 'ABCDEFGHJKLMNPQRSTUVXYWZIO';
+    const A1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3];
+    const A2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5];
+    const Mx = [9, 8, 7, 6, 5, 4, 3, 2, 1, 1];
+
+    if (id.length !== 10) return true;
+    let i = tab.indexOf(id.charAt(0));
+    if (i === -1) return true;
+    let sum = A1[i] + A2[i] * 9;
+
+    for (i = 1; i < 10; i++) {
+        let v = parseInt(id.charAt(i));
+        if (isNaN(v)) return true;
+        sum = sum + v * Mx[i];
+    }
+    if (sum % 10 !== 0) return true;
+    return false;
+}
